d48bd28cb7a9ae6e3b0d7e9f9a9aa6bf08b8ba11
Chiko <2452559902@qq.com>
2024-03-06 23:31:52 +0800

    add: data type

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ import './assets/css/style.css';
 import Icon from "/public/image/icon.png";
 import Data from "./assets/text/data.xml";
 import Notes from "./assets/text/data.csv";
+import toml from './assets/text/data.toml';
+import yaml from './assets/text/data.yaml';
+import json from './assets/text/data.json5';
 
+console.log(toml.title); // 输出 `TOML Example`
+console.log(toml.owner.name); // 输出 `Tom Preston-Werner`
+
+console.log(yaml.title); // 输出 `YAML Example`
+console.log(yaml.owner.name); // 输出 `Tom Preston-Werner`
+
+console.log(json.title); // 输出 `JSON5 Example`
+console.log(json.owner.name); // 输出 `Tom Preston-Werner`
 // 这样直接引入是没办执行的
 
 function component() {
@@ -22,7 +33,7 @@ function component() {
 
   console.log(Data);
   console.log(Notes);
-  
+
   return element;
 }
 
